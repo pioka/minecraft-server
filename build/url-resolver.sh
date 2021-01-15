09911b5c8dd2b3a9306684587a25f8ed094b427e
pioka <p10mirror2@gmail.com>
2021-01-16 04:51:22 +0900

    不要になったファイルを削除

--- build/url-resolver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-LIST_URL='http://launchermeta.mojang.com/mc/game/version_manifest.json'
-
-VERSION=${1:?}
-
-MANIFEST_URL=`curl -s $LIST_URL | jq -r ".versions[] | select(.id==\"$VERSION\") .url"`
-JAR_URL=`curl -s $MANIFEST_URL | jq -r ".downloads .server .url"`
-
-echo $JAR_URL
